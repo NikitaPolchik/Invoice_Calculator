8a2857aa5d72d481f1246384abc55cc37080a5b3
Polchik Nikita <polchik.nikita@gmail.com>
2023-12-28 23:51:57 -0800

    UI redesigned

--- Invoice_Calculator.py
+++ Invoice_Calculator.py
@@ -12,45 +12,44 @@ class StylesLayout(Widget):
     green_color = [0, 1, 0]
     default_color = [0.3, 0.3, 0.3]
 
-    def change_repeat_customer_button_color(self, value):
-            self.repeat_customer_value = value
-            if value == "yes":
-                self.ids.yes_customer_button.background_color = self.green_color
-                self.ids.no_customer_button.background_color = self.default_color
-            elif value == "no":
-                self.ids.yes_customer_button.background_color = self.default_color
-                self.ids.no_customer_button.background_color = self.red_color
+    def change_repeat_customer_button_state(self):
+            if self.ids.repeat_customer_button.state == "down":
+                self.ids.repeat_customer_button.background_color = self.green_color
+            else:
+                self.ids.repeat_customer_button.background_color = self.default_color
+
+    def change_shipping_button_state(self):
+        if self.ids.shipping_button.state == "down":
+            self.ids.shipping_button.background_color = self.green_color
+        else:
+            self.ids.shipping_button.background_color = self.default_color
+
     
-    def change_parts_button_color(self, value):
-        self.parts_button_value = value
-        if value == "1":
+    def change_parts_button_state(self):
+        if self.ids.one_part_button.state == "down":
             self.ids.one_part_button.background_color = self.green_color
             self.ids.two_parts_button.background_color = self.default_color
             self.ids.three_parts_button.background_color = self.default_color
-        elif value == "2":
+        elif self.ids.two_parts_button.state == "down":
             self.ids.one_part_button.background_color = self.default_color
             self.ids.two_parts_button.background_color = self.green_color
             self.ids.three_parts_button.background_color = self.default_color
-        elif value == "3":
+        elif self.ids.three_parts_button.state == "down":
             self.ids.one_part_button.background_color = self.default_color
             self.ids.two_parts_button.background_color = self.default_color
             self.ids.three_parts_button.background_color = self.green_color
+        else:
+            self.ids.one_part_button.background_color = self.default_color
+            self.ids.two_parts_button.background_color = self.default_color
+            self.ids.three_parts_button.background_color = self.default_color
 
-    def change_workers_button_color(self, value):
-            self.workers_value = value
-            if value == "yes":
-                self.ids.yes_workers_button.background_color = self.green_color
-                self.ids.no_workers_button.background_color = self.default_color
-            elif value == "no":
-                self.ids.yes_workers_button.background_color = self.default_color
-                self.ids.no_workers_button.background_color = self.red_color
-
-
+    def change_worker_button_state(self):
+            if self.ids.workers_button.state == "down":
+                self.ids.workers_button.background_color = self.green_color
+            else:
+                self.ids.workers_button.background_color = self.default_color
 
     def submit(self):
-        self.change_workers_button_color("")
-        self.change_repeat_customer_button_color("")
-        self.change_parts_button_color("")
         try:
             main_price = float(self.ids.main_price.text)
             print(main_price)
@@ -63,12 +62,11 @@ class StylesLayout(Widget):
             return str(int(float_number * multiplier)/multiplier)
         
         sellers_tax = 8.75
-        shipping = 20
         multiplication_index = 8
-
+        
         #labor
         labor_cost = round(main_price/100*40)
-        if self.workers_value == "yes":
+        if self.ids.workers_button.state == "down":
             labor_cost = 210
         elif labor_cost < 149:
             labor_cost = 149
@@ -76,15 +74,21 @@ class StylesLayout(Widget):
             labor_cost = 179
         labor_discount = labor_cost/10
 
+        #shipping
+        if self.ids.shipping_button.state == "down":
+            shipping = 20
+        else:
+            shipping = 0
+
         main_part_price = main_price - labor_cost - shipping
-        first_part = 0
+        """first_part = 0
         second_part = 0
-        third_part = 0
+        third_part = 0"""
         tax_inside_a_price = main_part_price/100*multiplication_index
         total_parts_cost = (main_price - labor_cost - shipping)-tax_inside_a_price
         part_tax = total_parts_cost/100*sellers_tax
 
-        if self.repeat_customer_value == "yes":
+        if self.ids.repeat_customer_button.state == "down":
             total_parts_cost += labor_discount
         else:
             labor_discount = 0
@@ -93,12 +97,12 @@ class StylesLayout(Widget):
         total_price = total_parts_cost + part_tax + labor_cost + shipping - labor_discount
 
         #output
-        if self.parts_button_value == "2":
+        if self.ids.two_parts_button.state == "down":
             first_part = main_part_price/100*60
             second_part = main_part_price/100*40
             print("first part price: " + truncate(first_part))
             print("second part price: " + truncate(second_part))
-        elif self.parts_button_value == "3":
+        elif self.ids.three_parts_button.state == "down":
             first_part = main_part_price/100*50
             second_part = main_part_price/100*30
             third_part = main_part_price/100*20
